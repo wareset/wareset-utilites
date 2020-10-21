7203a52e4b96d3da12b27121b664bab466524f33
wareset <git@wareset.com>
2020-10-21 12:13:33 +0500

    remove dom

--- packages/dom/src/index.mjs
+++
@@ -1,9 +0,0 @@
-import html from './parser/html/index.mjs';
-import concise from './parser/concise/index.mjs';
-import attributes from './parser/attributes/index.mjs';
-
-export { html, concise, attributes };
-
-export const parser = { html, concise, attributes };
-
-
--- packages/dom/src/parser/concise/index.mjs
+++
@@ -1,1 +0,0 @@
-export const parseConcise = (html, options) => {};
