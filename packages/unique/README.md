bfa458540a460595d9dff8e1c8ee1e48e4ea254e
wareset <git@wareset.com>
2021-01-23 00:32:12 +0500

    @wareset-utilites/unique: v1.1.0

--- packages/unique/README.md
+++ packages/unique/README.md
@@ -1,48 +1,5 @@
 # `@wareset-utilites/unique`
 
-Array filter
-
-## Installation
-
-```bash
-npm i @wareset-utilites/unique
-```
-
-or
-
-```bash
-yarn add @wareset-utilites/unique
-```
-
-## Usage
-
-### Require or Import:
-
-```js
-const unique = require('@wareset-utilites/unique');
-// or
-import unique from '@wareset-utilites/unique';
-```
-
-#### Mathod: unique.bind(filter: [] = ['', null, undefined])
-
-```js
-let arr = [0, 1, 2, 3, 1, 2, 3, '', null, undefined];
-
-console.log(arr.filter(unique)), /* OR */ console.log(unique(arr));
-/* RETURN: */ [0, 1, 2, 3];
-
-// because filter (bind) by default ['', null, undefined];
-
-console.log(arr.filter(unique.bind([1, 2])));
-/* RETURN: */ [0, 3, '', null, undefined];
-
-arr = ['foo', 'bar', 'baz'];
-
-console.log(arr.filter(unique.bind([/^ba/])));
-/* RETURN: */ ['foo'];
-```
-
 ## License
 
 MIT
---
+++ packages/unique/src/index.ts
@@ -0,0 +1,35 @@
+// function globals() {
+//   return typeof globalThis !== 'undefined'
+//     ? globalThis
+//     : typeof global !== 'undefined'
+//     ? global
+//     : this
+// }
+// const __global__ = globals()
+
+// function _filter(v, k, a) {
+//   for (const val of this) {
+//     if (v === val) return false
+//     if (val instanceof RegExp && val.test('' + v)) return false
+//   }
+//   return a.indexOf(v) === k
+// }
+
+// const DEFAULT_PATTERN = ['', null, undefined]
+
+// export default function unique(arr, k, a) {
+//   let self = this
+//   if (this === __global__ || !Array.isArray(this)) self = DEFAULT_PATTERN
+//   const fn = _filter.bind(self)
+//   return a ? fn(arr, k, a) : arr.filter(fn)
+// }
+
+// import { indexOf, includes } from '@wareset-utilites/lang'
+
+export const unique = (
+  array: any[],
+  filter: any[] = ['', NaN, null, undefined]
+): any[] =>
+  array.filter((v, k, a) => a.indexOf(v) === k && filter.indexOf(v) < 0)
+
+export default unique
