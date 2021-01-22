06841ccb6618ee42456ef6a36169bac5e6ded775
wareset <git@wareset.com>
2021-01-22 23:45:08 +0500

    @wareset-utilites/typed: v0.3.1

--- packages/typed/__tests__/index.test.js
+++ packages/typed/__tests__/index.test.js
@@ -1,204 +1,204 @@
-const typed = require('../dist/index').default;
+const typed = require('../dist/index').default
 
 test('Infinity:', () => {
-  expect(typed(Infinity)).toBe(Number);
-  expect(typed.of(Infinity)).toEqual([Number, Object, null]);
+  expect(typed(Infinity)).toBe(Number)
+  expect(typed.of(Infinity)).toEqual([Number, Object, null])
 
-  expect(typed(Infinity, Number)).toBe(true);
-  expect(typed(Infinity, Number, String)).toBe(true);
-  expect(typed(Infinity, String)).toBe(false);
-  expect(typed(Infinity, Array, String)).toBe(false);
+  expect(typed(Infinity, Number)).toBe(true)
+  expect(typed(Infinity, Number, String)).toBe(true)
+  expect(typed(Infinity, String)).toBe(false)
+  expect(typed(Infinity, Array, String)).toBe(false)
 
-  expect(typed.of(Infinity, null)).toBe(true);
-  expect(typed.of(Infinity, Object, Array, String)).toBe(true);
-  expect(typed.of(Infinity, undefined, Array, String)).toBe(false);
-});
+  expect(typed.of(Infinity, null)).toBe(true)
+  expect(typed.of(Infinity, Object, Array, String)).toBe(true)
+  expect(typed.of(Infinity, undefined, Array, String)).toBe(false)
+})
 
 test('NaN:', () => {
-  expect(typed(NaN)).toBe(Number);
-  expect(typed.of(Infinity)).toEqual([Number, Object, null]);
-});
+  expect(typed(NaN)).toBe(Number)
+  expect(typed.of(Infinity)).toEqual([Number, Object, null])
+})
 
 test('undefined:', () => {
-  expect(typed(undefined)).toBe(undefined);
-  expect(typed.of(undefined)).toEqual([undefined]);
-});
+  expect(typed(undefined)).toBe(undefined)
+  expect(typed.of(undefined)).toEqual([undefined])
+})
 
 test('null:', () => {
-  expect(typed(null)).toBe(null);
-  expect(typed.of(null)).toEqual([null]);
-});
+  expect(typed(null)).toBe(null)
+  expect(typed.of(null)).toEqual([null])
+})
 
 test('Object:', () => {
-  const object = {};
-  expect(typed(object)).toBe(Object);
-  expect(typed.of(object)).toEqual([Object, null]);
-});
+  const object = {}
+  expect(typed(object)).toBe(Object)
+  expect(typed.of(object)).toEqual([Object, null])
+})
 
 test('ObjectCreateNull:', () => {
-  const object = Object.create(null);
-  expect(typed(object)).toBe(null);
-  expect(typed.of(object)).toEqual([null]);
-});
+  const object = Object.create(null)
+  expect(typed(object)).toBe(null)
+  expect(typed.of(object)).toEqual([null])
+})
 
 test('ObjectCreateCustom:', () => {
-  const proto = { q: 1 };
-  const object = Object.create(proto);
-  expect(typed(object)).toBe(proto);
-  expect(typed.of(object)).toEqual([proto, Object, null]);
-  expect(typed(object, proto)).toBe(true);
+  const proto = { q: 1 }
+  const object = Object.create(proto)
+  expect(typed(object)).toBe(proto)
+  expect(typed.of(object)).toEqual([proto, Object, null])
+  expect(typed(object, proto)).toBe(true)
 
-  const object2 = Object.create(object);
-  expect(typed(object2)).toEqual({});
-  expect(typed.of(object2)).toEqual([{}, { q: 1 }, Object, null]);
-  expect(typed(object2, object)).toBe(true);
-  expect(typed(object2, proto)).toBe(false);
-  expect(typed.of(object2, proto)).toBe(true);
+  const object2 = Object.create(object)
+  expect(typed(object2)).toEqual({})
+  expect(typed.of(object2)).toEqual([{}, { q: 1 }, Object, null])
+  expect(typed(object2, object)).toBe(true)
+  expect(typed(object2, proto)).toBe(false)
+  expect(typed.of(object2, proto)).toBe(true)
 
-  const proto2 = [1, 2, 3];
-  const object3 = Object.create(proto2);
-  expect(typed(object3)).toBe(proto2);
-  expect(typed.of(object3)).toEqual([proto2, Array, Object, null]);
-  expect(typed(object3, proto2)).toBe(true);
-  expect(typed(object3, Object)).toBe(false);
+  const proto2 = [1, 2, 3]
+  const object3 = Object.create(proto2)
+  expect(typed(object3)).toBe(proto2)
+  expect(typed.of(object3)).toEqual([proto2, Array, Object, null])
+  expect(typed(object3, proto2)).toBe(true)
+  expect(typed(object3, Object)).toBe(false)
 
-  const object4 = Object.create(object3);
-  expect(typed(object4)).toBe(object3);
-  expect(typed.of(object4)).toEqual([{}, proto2, Array, Object, null]);
-  expect(typed(object4, object3)).toBe(true);
-  expect(typed.of(object4, proto2)).toBe(true);
-});
+  const object4 = Object.create(object3)
+  expect(typed(object4)).toBe(object3)
+  expect(typed.of(object4)).toEqual([{}, proto2, Array, Object, null])
+  expect(typed(object4, object3)).toBe(true)
+  expect(typed.of(object4, proto2)).toBe(true)
+})
 
 test('Function:', () => {
-  expect(typed(() => {})).toBe(Function);
-  expect(typed.of(() => {})).toEqual([Function, Object, null]);
+  expect(typed(() => {})).toBe(Function)
+  expect(typed.of(() => {})).toEqual([Function, Object, null])
 
-  const AsyncFunction = (async () => {}).constructor;
-  expect(typed(async () => {})).toBe(AsyncFunction);
+  const AsyncFunction = (async () => {}).constructor
+  expect(typed(async () => {})).toBe(AsyncFunction)
   expect(typed.of(async () => {})).toEqual([
     AsyncFunction,
     Function,
     Object,
     null
-  ]);
+  ])
 
   function* gen() {}
-  const GeneratorFunction = gen.constructor;
-  expect(typed(gen)).toBe(GeneratorFunction);
-  expect(typed.of(gen)).toEqual([GeneratorFunction, Function, Object, null]);
-});
+  const GeneratorFunction = gen.constructor
+  expect(typed(gen)).toBe(GeneratorFunction)
+  expect(typed.of(gen)).toEqual([GeneratorFunction, Function, Object, null])
+})
 
 test('Boolean:', () => {
-  const boolean = true;
-  expect(typed(boolean)).toBe(Boolean);
-  expect(typed.of(boolean)).toEqual([Boolean, Object, null]);
-});
+  const boolean = true
+  expect(typed(boolean)).toBe(Boolean)
+  expect(typed.of(boolean)).toEqual([Boolean, Object, null])
+})
 
 test('Symbol:', () => {
-  const symbol = Symbol('1');
-  expect(typed(symbol)).toBe(Symbol);
-  expect(typed.of(symbol)).toEqual([Symbol, Object, null]);
-});
+  const symbol = Symbol('1')
+  expect(typed(symbol)).toBe(Symbol)
+  expect(typed.of(symbol)).toEqual([Symbol, Object, null])
+})
 
 test('Error:', () => {
-  const error = new Error('Error text', 'someFile.js', 10);
-  expect(typed(error)).toBe(Error);
-  expect(typed.of(error)).toEqual([Error, Object, null]);
+  const error = new Error('Error text', 'someFile.js', 10)
+  expect(typed(error)).toBe(Error)
+  expect(typed.of(error)).toEqual([Error, Object, null])
 
-  const evalError = new EvalError('Error text', 'someFile.js', 10);
-  expect(typed(evalError)).toBe(EvalError);
-  expect(typed.of(evalError)).toEqual([EvalError, Error, Object, null]);
-});
+  const evalError = new EvalError('Error text', 'someFile.js', 10)
+  expect(typed(evalError)).toBe(EvalError)
+  expect(typed.of(evalError)).toEqual([EvalError, Error, Object, null])
+})
 
 test('Number:', () => {
-  const number = 304010;
-  expect(typed(number)).toBe(Number);
-  expect(typed.of(number)).toEqual([Number, Object, null]);
-});
+  const number = 304010
+  expect(typed(number)).toBe(Number)
+  expect(typed.of(number)).toEqual([Number, Object, null])
+})
 
 test('Date:', () => {
-  const date = new Date();
-  expect(typed(date)).toBe(Date);
-  expect(typed.of(date)).toEqual([Date, Object, null]);
-});
+  const date = new Date()
+  expect(typed(date)).toBe(Date)
+  expect(typed.of(date)).toEqual([Date, Object, null])
+})
 
 test('String:', () => {
-  const string = 'string';
-  expect(typed(string)).toBe(String);
-  expect(typed.of(string)).toEqual([String, Object, null]);
-});
+  const string = 'string'
+  expect(typed(string)).toBe(String)
+  expect(typed.of(string)).toEqual([String, Object, null])
+})
 
 test('RegExp:', () => {
-  const regexp = /\s*/g;
-  expect(typed(regexp)).toBe(RegExp);
-  expect(typed.of(regexp)).toEqual([RegExp, Object, null]);
-});
+  const regexp = /\s*/g
+  expect(typed(regexp)).toBe(RegExp)
+  expect(typed.of(regexp)).toEqual([RegExp, Object, null])
+})
 
 test('Array:', () => {
-  const array = [1, 2, 3];
-  expect(typed(array)).toBe(Array);
-  expect(typed.of(array)).toEqual([Array, Object, null]);
-});
+  const array = [1, 2, 3]
+  expect(typed(array)).toBe(Array)
+  expect(typed.of(array)).toEqual([Array, Object, null])
+})
 
 test('ArrayBuffer:', () => {
-  const arrayBuffer = new ArrayBuffer(16);
-  expect(typed(arrayBuffer)).toBe(ArrayBuffer);
+  const arrayBuffer = new ArrayBuffer(16)
+  expect(typed(arrayBuffer)).toBe(ArrayBuffer)
   // expect(typed.of(arrayBuffer)).toEqual([ArrayBuffer, Object, null]);
-  expect(typed(arrayBuffer, ArrayBuffer)).toBe(true);
+  expect(typed(arrayBuffer, ArrayBuffer)).toBe(true)
   // expect(typed.of(arrayBuffer, Object)).toBe(true);
-  expect(typed.of(arrayBuffer, null)).toBe(true);
-});
+  expect(typed.of(arrayBuffer, null)).toBe(true)
+})
 
 test('DataView:', () => {
-  const dataView = new DataView(new ArrayBuffer(16));
-  expect(typed(dataView)).toBe(DataView);
-  expect(typed.of(dataView)).toEqual([DataView, Object, null]);
-});
+  const dataView = new DataView(new ArrayBuffer(16))
+  expect(typed(dataView)).toBe(DataView)
+  expect(typed.of(dataView)).toEqual([DataView, Object, null])
+})
 
 test('Map:', () => {
-  const map = new Map();
-  expect(typed(map)).toBe(Map);
-  expect(typed.of(map)).toEqual([Map, Object, null]);
-});
+  const map = new Map()
+  expect(typed(map)).toBe(Map)
+  expect(typed.of(map)).toEqual([Map, Object, null])
+})
 
 test('Set:', () => {
-  const set = new Set();
-  expect(typed(set)).toBe(Set);
-  expect(typed.of(set)).toEqual([Set, Object, null]);
-});
+  const set = new Set()
+  expect(typed(set)).toBe(Set)
+  expect(typed.of(set)).toEqual([Set, Object, null])
+})
 
 test('WeakMap:', () => {
-  const map = new WeakMap();
-  expect(typed(map)).toBe(WeakMap);
-  expect(typed.of(map)).toEqual([WeakMap, Object, null]);
-});
+  const map = new WeakMap()
+  expect(typed(map)).toBe(WeakMap)
+  expect(typed.of(map)).toEqual([WeakMap, Object, null])
+})
 
 test('WeakSet:', () => {
-  const set = new WeakSet();
-  expect(typed(set)).toBe(WeakSet);
-  expect(typed.of(set)).toEqual([WeakSet, Object, null]);
-});
+  const set = new WeakSet()
+  expect(typed(set)).toBe(WeakSet)
+  expect(typed.of(set)).toEqual([WeakSet, Object, null])
+})
 
 test('Class:', () => {
   class Qwer {}
   class Wert extends Qwer {}
   class Erty extends Wert {}
 
-  const newClass = new Erty();
-  expect(typed(newClass)).toBe(Erty);
-  expect(typed.of(newClass)).toEqual([Erty, Wert, Qwer, Object, null]);
-});
+  const newClass = new Erty()
+  expect(typed(newClass)).toBe(Erty)
+  expect(typed.of(newClass)).toEqual([Erty, Wert, Qwer, Object, null])
+})
 
-test('DIV:', () => {
-  const div = document.createElement('div');
-  expect(typed(div)).toBe(HTMLDivElement);
-  // expect(typed.of(div)).toEqual([
-  //   HTMLDivElement,
-  //   HTMLElement,
-  //   Element,
-  //   Node,
-  //   EventTarget,
-  //   Object,
-  //   null
-  // ]);
-});
+// test('DIV:', () => {
+//   const div = document.createElement('div')
+//   expect(typed(div)).toBe(HTMLDivElement)
+//   // expect(typed.of(div)).toEqual([
+//   //   HTMLDivElement,
+//   //   HTMLElement,
+//   //   Element,
+//   //   Node,
+//   //   EventTarget,
+//   //   Object,
+//   //   null
+//   // ]);
+// })
--- packages/typed/src/index.ts
+++ packages/typed/src/index.ts
@@ -1,62 +1,52 @@
-const getProto = Object.getPrototypeOf || ((v: any): any => v.__proto__);
-const getPrototypeOf = (v: any): any => (v == null ? v : getProto(v));
+const getProto = Object.getPrototypeOf || ((v: any): any => v.__proto__)
+const getPrototypeOf = (v: any): any => (v == null ? v : getProto(v))
 const getProtoFn = (v: any): any =>
-  !v || v.constructor.prototype !== v ? v : v.constructor;
+  !v || !v.constructor || v.constructor.prototype !== v ? v : v.constructor
 
 const getCtor = (v: any): Function | object =>
-  (v = getProtoFn(getPrototypeOf(v)));
+  (v = getProtoFn(getPrototypeOf(v)))
 const getCtors = (v: any): Array<Function | object> => {
-  const protos = [];
-  do (v = getPrototypeOf(v)), protos.push(getProtoFn(v));
-  while (v);
-  return protos;
-};
+  const protos = []
+  do (v = getPrototypeOf(v)), protos.push(getProtoFn(v))
+  while (v)
+  return protos
+}
 
 const eq = (a: any, b: any): boolean =>
-  a === b || (a === getCtor(b) && (!b.prototype || typeof b !== 'function'));
+  a === b || (a === getCtor(b) && (!b.prototype || typeof b !== 'function'))
 // const cases = (t) =>
 //   getCtor(t[0]) === Array && t[0].length ? [...t.shift(), ...t] : t;
 
 const check = (fn: Function) => (value: any, ...t: any[]): any => {
-  if (t.length && !fn(value, ...t)) throw new TypeError(value);
-  return value;
-};
-
-// eslint-disable-next-line no-redeclare
-function typedOf(value: any): Array<Function | object | null | undefined>;
-// eslint-disable-next-line no-redeclare
-function typedOf(
-  value: any,
-  ...types: Array<Function | object | null | undefined>
-): boolean;
-// eslint-disable-next-line no-redeclare
-function typedOf(
-  value: any,
-  ...types: Array<Function | object | null | undefined>
-): Array<Function | object | null | undefined> | boolean {
-  const ctors = getCtors(value);
-  if (!types.length) return ctors;
-  return ctors.some((ctor) => types.some((t) => eq(ctor, t)));
+  if (t.length && !fn(value, ...t)) throw new TypeError(value)
+  return value
 }
-typedOf.check = check(typedOf);
 
-// eslint-disable-next-line no-redeclare
-function typed(value: any): Function | object | null | undefined;
-// eslint-disable-next-line no-redeclare
-function typed(
-  value: any,
-  ...types: Array<Function | object | null | undefined>
-): boolean;
-// eslint-disable-next-line no-redeclare
-function typed(
-  value: any,
-  ...types: Array<Function | object | null | undefined>
-): Function | object | null | undefined | boolean {
-  const ctor = getCtor(value);
-  return !types.length ? ctor : types.some((t) => eq(ctor, t));
+type IPrototype = Function | object | null | undefined
+type IPrototypesList = IPrototype[]
+
+const typedOf: {
+  (value: any): IPrototypesList
+  (value: any, ...types: IPrototypesList): boolean
+  check: typeof check
+} = (value: any, ...types: IPrototypesList): any => {
+  const ctors = getCtors(value)
+  if (!types.length) return ctors
+  return ctors.some((ctor) => types.some((t) => eq(ctor, t)))
 }
-typed.check = check(typed);
-typed.of = typedOf;
+typedOf.check = check(typedOf)
 
-export default typed;
-export { typed, typedOf };
+const typed: {
+  (value: any): IPrototype
+  (value: any, ...types: IPrototypesList): boolean
+  check: typeof check
+  of: typeof typedOf
+} = (value: any, ...types: IPrototypesList): any => {
+  const ctor = getCtor(value)
+  return !types.length ? ctor : types.some((t) => eq(ctor, t))
+}
+typed.check = check(typed)
+typed.of = typedOf
+
+export { typed, typedOf }
+export default typed
