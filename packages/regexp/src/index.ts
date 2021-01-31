fcc4f5eb207c811869c926babb2cb0cfa09b5667
wareset <git@wareset.com>
2021-02-01 00:50:00 +0500

    autobuild: regexp/0.1.4

--- packages/regexp/src/index.ts
+++ packages/regexp/src/index.ts
@@ -1,8 +1,8 @@
-import { test, last } from '@wareset-utilites/lang'
+import { test, last, size } from '@wareset-utilites/lang'
 
 export const regexp = (...a: Array<string | RegExp>): RegExp => {
   const flags: any =
-    a.length < 2 || last(a).source || !test(/^[gim]+$/, last(a)) ? '' : a.pop()
+    size(a) < 2 || last(a).source || !test(/^[gim]+$/, last(a)) ? '' : a.pop()
   return new RegExp(a.map((v) => (v as RegExp).source || v).join(''), flags)
 }
 
