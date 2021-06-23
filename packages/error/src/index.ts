be4676ad7afc88c4186efab08ab1a5fa8813ef19
wareset <git@wareset.com>
2021-06-24 00:26:08 +0500

    autobuild: error/0.1.2

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,34 +1,35 @@
 /* eslint-disable max-len */
 
+export const ErrorClass = Error
+export const EvalErrorClass = EvalError
+export const RangeErrorClass = RangeError
+export const ReferenceErrorClass = ReferenceError
+export const SyntaxErrorClass = SyntaxError
+export const TypeErrorClass = TypeError
+export const URIErrorClass = URIError
+
 const __run__ = (ErrorFn: any, ...a: any[]): never => {
   throw new ErrorFn(...a)
 }
 
 // prettier-ignore
-export const error: {
-  (...a: any[]): never
-} = ((...a: any[]) => { __run__(Error, ...a) as never }) as (...a: any[]) => never
+export const throwError =
+  ((...a: any[]) => { __run__(ErrorClass, ...a) }) as (...a: any[]) => never
 // prettier-ignore
-export const evalError: {
-  (...a: any[]): never
-} = ((...a: any[]) => { __run__(EvalError, ...a) }) as (...a: any[]) => never
+export const throwEvalError =
+  ((...a: any[]) => { __run__(EvalErrorClass, ...a) }) as (...a: any[]) => never
 // prettier-ignore
-export const rangeError: {
-  (...a: any[]): never
-} = ((...a: any[]) => { __run__(RangeError, ...a) }) as (...a: any[]) => never
+export const throwRangeError =
+  ((...a: any[]) => { __run__(RangeErrorClass, ...a) }) as (...a: any[]) => never
 // prettier-ignore
-export const referenceError: {
-  (...a: any[]): never
-} = ((...a: any[]) => { __run__(ReferenceError, ...a) }) as (...a: any[]) => never
+export const throwReferenceError =
+  ((...a: any[]) => { __run__(ReferenceErrorClass, ...a) }) as (...a: any[]) => never
 // prettier-ignore
-export const syntaxError: {
-  (...a: any[]): never
-} = ((...a: any[]) => { __run__(SyntaxError, ...a) }) as (...a: any[]) => never
+export const throwSyntaxError =
+  ((...a: any[]) => { __run__(SyntaxErrorClass, ...a) }) as (...a: any[]) => never
 // prettier-ignore
-export const typeError: {
-  (...a: any[]): never
-} = ((...a: any[]) => { __run__(TypeError, ...a) }) as (...a: any[]) => never
+export const throwTypeError =
+  ((...a: any[]) => { __run__(TypeErrorClass, ...a) }) as (...a: any[]) => never
 // prettier-ignore
-export const uriError: {
-  (...a: any[]): never
-} = ((...a: any[]) => { __run__(URIError, ...a) }) as (...a: any[]) => never
+export const throwURIError =
+  ((...a: any[]) => { __run__(URIErrorClass, ...a) }) as (...a: any[]) => never
