85cb0f2f3f4084af01674d93ed2637a800f31e91
wareset <git@wareset.com>
2021-07-18 04:43:29 +0500

    autobuild: queuer/0.2.2

--- packages/queuer/index.d.ts
+++ packages/queuer/index.d.ts
@@ -1,6 +1,4 @@
-export * from './__types__';
-
-export { Queuer } from './__types__';
-
-import __default__ from './__types__';
+export * from "./__types__";
+export { Queuer } from "./__types__";
+import __default__ from "./__types__";
 export { __default__ as default };
