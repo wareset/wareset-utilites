55febb7d099b0d80c346c4e8236a2dbd68b464e2
wareset <git@wareset.com>
2021-08-10 04:14:58 +0500

    autobuild: stacktrace/0.1.6

--- packages/stacktrace/index.js
+++ packages/stacktrace/index.js
@@ -2,41 +2,7 @@
 
 Object.defineProperty(exports, '__esModule', {
   value: true
-}); // const ERROR_STRING = () => String(Error().stack)
-// let BASE_STACK = []
-// const stack = () =>
-//   ERROR_STRING()
-//     .split(/\n+/g)
-//     .map((v) => v.trim())
-//     .filter((v) => BASE_STACK.indexOf(v) === -1)
-// const DEFAULT_STACK = {
-//   __stack__: [],
-//   get stack() {
-//     return stack()
-//   }
-// }
-// export const stacktrace = (() => {
-//   BASE_STACK = DEFAULT_STACK.stack
-//   return (verbose) => {
-//     let text = stack().reverse()
-//     text.pop()
-//     if (!verbose) {
-//       text = text.map((v) => v.split(/:/g).slice(0, -2).join(':'))
-//     }
-//     if (verbose === 1) {
-//       text = text.map((v) =>
-//         v
-//           .split(/:/g)
-//           .slice(-2)
-//           .join(':')
-//           .replace(/[^\d:]/g, '')
-//       )
-//     } else text = text.map((v, k, a) => k + '__' + v)
-//     if (!text.length) text.push('0__global')
-//     return text
-//   }
-// })()
-// export default stacktrace
+});
 
 var stacktrace = () => ('' + Error().stack).split(/\s*\n+\s*/).slice(2);
 
