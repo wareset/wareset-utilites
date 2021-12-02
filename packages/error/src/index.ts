0e5b2536919d04335bae8cda4330a1d72dfb4d12
wareset <git@wareset.com>
2021-12-02 11:11:09 +0500

    autosave: error

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-
 export const ErrorClass = Error
 export const EvalErrorClass = EvalError
 export const RangeErrorClass = RangeError
@@ -12,24 +10,31 @@ const __run__ = (ErrorFn: any, ...a: any[]): never => {
   throw new ErrorFn(...a)
 }
 
-// prettier-ignore
 export const throwError =
-  ((...a: any[]) => { __run__(ErrorClass, ...a) }) as (...a: any[]) => never
-// prettier-ignore
+  ((...a: any[]) => {
+    __run__(ErrorClass, ...a)
+  }) as (...a: any[]) => never
 export const throwEvalError =
-  ((...a: any[]) => { __run__(EvalErrorClass, ...a) }) as (...a: any[]) => never
-// prettier-ignore
+  ((...a: any[]) => {
+    __run__(EvalErrorClass, ...a)
+  }) as (...a: any[]) => never
 export const throwRangeError =
-  ((...a: any[]) => { __run__(RangeErrorClass, ...a) }) as (...a: any[]) => never
-// prettier-ignore
+  ((...a: any[]) => {
+    __run__(RangeErrorClass, ...a)
+  }) as (...a: any[]) => never
 export const throwReferenceError =
-  ((...a: any[]) => { __run__(ReferenceErrorClass, ...a) }) as (...a: any[]) => never
-// prettier-ignore
+  ((...a: any[]) => {
+    __run__(ReferenceErrorClass, ...a)
+  }) as (...a: any[]) => never
 export const throwSyntaxError =
-  ((...a: any[]) => { __run__(SyntaxErrorClass, ...a) }) as (...a: any[]) => never
-// prettier-ignore
+  ((...a: any[]) => {
+    __run__(SyntaxErrorClass, ...a)
+  }) as (...a: any[]) => never
 export const throwTypeError =
-  ((...a: any[]) => { __run__(TypeErrorClass, ...a) }) as (...a: any[]) => never
-// prettier-ignore
+  ((...a: any[]) => {
+    __run__(TypeErrorClass, ...a)
+  }) as (...a: any[]) => never
 export const throwURIError =
-  ((...a: any[]) => { __run__(URIErrorClass, ...a) }) as (...a: any[]) => never
+  ((...a: any[]) => {
+    __run__(URIErrorClass, ...a)
+  }) as (...a: any[]) => never
