e59eb563b978bc568e1b528f339726b3944ff636
wareset <git@wareset.com>
2021-12-02 11:10:50 +0500

    autosave: deep-equal

--- packages/deep-equal/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./__types__";
-export { deepEqual } from "./__types__";
-export { deepEqualExtended } from "./__types__";
-import __default__ from "./__types__";
-export { __default__ as default };
--- packages/deep-equal/src/index.ts
+++ packages/deep-equal/src/index.ts
@@ -1,11 +1,10 @@
-/* eslint-disable max-len */
-
 import { isNumber } from '@wareset-utilites/is/isNumber'
 import { isObject } from '@wareset-utilites/is/isObject'
 import { isFunction } from '@wareset-utilites/is/isFunction'
 import { isNativeFunction } from '@wareset-utilites/is/isNativeFunction'
 
 import { instanceOf } from '@wareset-utilites/lang/instanceOf'
+import { PROTOTYPE } from '@wareset-utilites/lang/lib'
 
 import { typed, typedOf } from '@wareset-utilites/typed'
 
@@ -14,7 +13,6 @@ import { Object as __Object__ } from '@wareset-utilites/object/Object'
 import { getOwnPropertyNames } from '@wareset-utilites/object/getOwnPropertyNames'
 import { getOwnPropertySymbols } from '@wareset-utilites/object/getOwnPropertySymbols'
 
-const prototype = 'prototype'
 const valueOf = 'valueOf'
 const toString = 'toString'
 
@@ -23,9 +21,9 @@ const getProtoOwnPropNames = (
   keys: string[],
   natives: boolean | undefined
 ): void => {
-  ;(typedOf(a) as Function[]).forEach((p) => {
+  (typedOf(a) as Function[]).forEach((p) => {
     if (p && (natives || !isNativeFunction(p))) {
-      keys.push(...getOwnPropertyNames(p[prototype]))
+      keys.push(...getOwnPropertyNames(p[PROTOTYPE]))
     }
   })
 }
@@ -39,22 +37,18 @@ export type TypeDeepEqualOptions = {
 }
 
 const OPTIONS: TypeDeepEqualOptions = {
-  depth: true,
+  depth  : true,
   symbols: true,
   immerse: true,
   noweaks: false,
   natives: false
 }
 
-const __ArrayBuffer__ = ArrayBuffer
-const __DataView__ = DataView
-const __Float64Array__ = Float64Array
-
 const __deepEqual__ = (
   a: any,
   b: any,
   depth: boolean | number | undefined,
-  options: TypeDeepEqualOptions = OPTIONS,
+  options: TypeDeepEqualOptions,
   __cache__: Map<any, any>
 ): boolean => {
   if (is(a, b)) return true
@@ -73,30 +67,41 @@ const __deepEqual__ = (
 
   let k, v, tmp: any
 
-  if (instanceOf(a, __ArrayBuffer__)) {
-    ;(a = new __DataView__(a as any)), (b = new __DataView__(b as any))
+  try {
+    if (instanceOf(a, ArrayBuffer)) {
+      a = new DataView(a as any); b = new DataView(b as any)
 
-    if (__ArrayBuffer__.isView(a)) {
-      if (a.byteLength !== b.byteLength) return false
-      a = new __Float64Array__(a.buffer)
-      b = new __Float64Array__(b.buffer)
-      for (k = a.byteLength; k-- > 0; ) if (a[k] !== b[k]) return false
-      return true
+      if (ArrayBuffer.isView(a)) {
+        if (a.byteLength !== b.byteLength) return false
+        a = new Float64Array(a.buffer); b = new Float64Array(b.buffer)
+        for (k = a.byteLength; k-- > 0;) if (a[k] !== b[k]) return false
+        return true
+      }
+      return false
     }
-    return false
+  } catch (e) {
+    /* */
   }
 
-  if ((tmp = instanceOf(a, Map)) || instanceOf(a, Set)) {
-    if (a.size !== (b as any).size) return false
+  try {
+    if ((tmp = instanceOf(a, Map)) || instanceOf(a, Set)) {
+      if (a.size !== (b as any).size) return false
 
-    if (tmp) {
-      for ([k] of a as any) if (!(b as any).has(k)) return false
-      for ([k, v] of a as any) if (!__da__(v, (b as any).get(k))) return false
-    } else for (v of a) if (!(b as any).has(v)) return false
+      if (tmp) {
+        for ([k] of a as any) if (!(b as any).has(k)) return false
+        for ([k, v] of a as any) if (!__da__(v, (b as any).get(k))) return false
+      } else for (v of a) if (!(b as any).has(v)) return false
+    }
+  } catch (e) {
+    /* */
   }
 
-  if (options.noweaks && (instanceOf(a, WeakMap) || instanceOf(a, WeakSet))) {
-    return false
+  try {
+    if (options.noweaks && (instanceOf(a, WeakMap) || instanceOf(a, WeakSet))) {
+      return false
+    }
+  } catch (e) {
+    /* */
   }
 
   let keys = getOwnPropertyNames(a)
@@ -105,23 +110,23 @@ const __deepEqual__ = (
     getProtoOwnPropNames(a, keys, options.natives)
   }
   if (instanceOf(a, Error)) keys = keys.filter((v: string) => v !== 'stack')
-  for (v of keys)
+  for (v of keys) {
     if (!(v in b) || !__da__((a as any)[v], (b as any)[v])) return false
+  }
 
   if (options.symbols && getOwnPropertySymbols) {
     const symbols = getOwnPropertySymbols(a)
     if (symbols.length !== getOwnPropertySymbols(b).length) return false
-    for (v of symbols)
+    for (v of symbols) {
       if (!(v in b) || !__da__((a as any)[v], (b as any)[v])) return false
+    }
   }
 
   try {
-    // prettier-ignore
-    if (a[valueOf] !== __Object__[prototype][valueOf] && isFunction(a[valueOf])) {
+    if (a[valueOf] !== __Object__[PROTOTYPE][valueOf] && isFunction(a[valueOf])) {
       return __da__(a[valueOf](), b[valueOf]())
     }
-    // prettier-ignore
-    if (a[toString] !== __Object__[prototype][toString] && isFunction(a[toString])) {
+    if (a[toString] !== __Object__[PROTOTYPE][toString] && isFunction(a[toString])) {
       return __da__(a[toString](), b[toString]())
     }
   } catch (err) {
