b5e849721abd2c101036b1c834e499c09ad96962
wareset <git@wareset.com>
2021-12-02 11:15:02 +0500

    autosave: queuer

--- packages/queuer/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./__types__";
-export { Queuer } from "./__types__";
-import __default__ from "./__types__";
-export { __default__ as default };
--- packages/queuer/src/index.ts
+++ packages/queuer/src/index.ts
@@ -4,22 +4,23 @@ import { isArray } from '@wareset-utilites/is/isArray'
 import { isPromise } from '@wareset-utilites/is/isPromise'
 import { isFunction } from '@wareset-utilites/is/isFunction'
 
+import { Promise } from '@wareset-utilites/lang/Promise'
+
 export default class Queuer {
   list: (Function | Function[])[] = []
   is = false
-
+  
   constructor(private res?: any) {}
-
+  
   run(): void {
     if (!this.is && this.list.length) {
       this.is = true
       const arr: any = this.list.shift()
       const tmp = (isArray(arr) ? [...arr] : [arr]).map((v) =>
-        isFunction(v) ? v(this.res) : v
-      )
+        isFunction(v) ? v(this.res) : v)
 
       const fin = (tmp: any): void => {
-        ;(this.res = last(tmp)), (this.is = false), this.run()
+        this.res = last(tmp), this.is = false, this.run()
       }
 
       tmp.some(isPromise) ? Promise.all(tmp).then(fin) : fin(tmp)
