21ac0c0ab9c80388e3b428a4f652a2f41caf51a6
wareset <git@wareset.com>
2021-12-02 11:15:21 +0500

    autosave: stacktrace

--- packages/stacktrace/__types__/index.d.ts
+++
@@ -1,2 +0,0 @@
-export function stacktrace(): string[];
-export default stacktrace;
--- packages/stacktrace/index.mjs
+++
@@ -1,4 +0,0 @@
-var stacktrace = () => ('' + Error().stack).split(/\s*\n+\s*/).slice(2);
-
-export default stacktrace;
-export { stacktrace };
