ba5a8fa4557fc9398e2e3cd22e5fb359ed0e1f0c
rease <cftcview@gmail.com>
2022-10-20 20:24:23 +0500

    remove unique

--- packages/unique/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable max-len */
-
-import unique from '../src'
-
-test('wareset-utilites: unique', () => {
-  expect(unique([1, 2, '', NaN, null, void 0, 1, 2, 3])).toEqual([1, 2, '', 3])
-  expect(unique([1, 2, '', NaN, null, 1], ['', null])).toEqual([1, 2, NaN])
-})
